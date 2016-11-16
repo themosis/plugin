c7ff90e0e38a66f8bb1dc4b10b235346bb105d99
Julien Lambé <julien@jlambe.be>
2016-11-16 12:12:17 +0100

    Add admin files autoloading and plugin textdomain language files.

--- plugin-name.php
+++ plugin-name.php
@@ -1,13 +1,15 @@
 <?php
 
-/*
-Plugin Name: Plugin Name
-Description: Plugin description.
-Author: Your name
-Version: 1.0.0
-Author URI: http://framework.themosis.com/
-Text Domain: plugin-name-textdomain
-*/
+/**
+ * Plugin Name: Plugin Name
+ * Plugin URI: http://www.domain.tld
+ * Description: Plugin description.
+ * Version: 1.0.0
+ * Author: Your name
+ * Author URI: http://framework.themosis.com/
+ * Text Domain: plugin-textdomain.
+ * Domain Path: /languages
+ */
 
 /**
  * Use statements. Add any useful class import
@@ -15,12 +17,12 @@ Text Domain: plugin-name-textdomain
  */
 use Composer\Autoload\ClassLoader;
 
-/**
+/*
  * Default constants.
  */
 defined('DS') ? DS : define('DS', DIRECTORY_SEPARATOR);
 
-/**
+/*
  * Always define the following constant to help you handle
  * your plugin text domain. Make sure to define the same value as
  * the one defined in the plugin header comments text domain.
@@ -28,25 +30,26 @@ defined('DS') ? DS : define('DS', DIRECTORY_SEPARATOR);
  * Please update (search and replace) all constants calls found in this file.
  *
  * TODO: #1
+ * TODO: #2 - Update plugin textdomain first argument below.
  */
-defined('MY_PLUGIN_TD') ? MY_PLUGIN_TD : define('MY_PLUGIN_TD', 'plugin-name-textdomain');
+defined('MY_PLUGIN_TD') ? MY_PLUGIN_TD : define('MY_PLUGIN_TD', 'plugin-textdomain');
 
-/**
+/*
  * Plugin variables.
  * Configure your plugin.
  *
- * TODO: #2 - Define your own values.
- * TODO: #3 - Update the config files names located into the `resources/config` folder.
- * TODO: #4 - Update class namespaces.
+ * TODO: #3 - Define your own values.
+ * TODO: #4 - Update the config files names located into the `resources/config` folder.
+ * TODO: #5 - Update class namespaces.
  */
 $vars = [
     'slug' => 'plugin-key',
     'name' => 'Plugin Name',
     'namespace' => 'tld.domain.plugin',
-    'config' => 'tld_domain_plugin'
+    'config' => 'tld_domain_plugin',
 ];
 
-/**
+/*
  * Verify that the main framework is loaded.
  */
 add_action('admin_notices', function () use ($vars) {
@@ -54,7 +57,7 @@ add_action('admin_notices', function () use ($vars) {
         printf('<div class="notice notice-error"><p>%s</p></div>', __('This plugin requires the Themosis framework in order to work.', MY_PLUGIN_TD));
     }
 
-    /**
+    /*
      * Define your plugin theme support key. Once defined, make sure to add the key
      * into your theme `supports.config.php` in order to remove this admin notice.
      */
@@ -63,7 +66,7 @@ add_action('admin_notices', function () use ($vars) {
     }
 });
 
-/**
+/*
  * Setup the plugin paths.
  */
 $paths['plugin.'.$vars['namespace']] = __DIR__.DS;
@@ -72,14 +75,14 @@ $paths['plugin.'.$vars['namespace'].'.admin'] = __DIR__.DS.'resources'.DS.'admin
 
 themosis_set_paths($paths);
 
-/**
+/*
  * Setup plugin config files.
  */
 container('config.finder')->addPaths([
-    themosis_path('plugin.'.$vars['namespace'].'.resources').'config'.DS
+    themosis_path('plugin.'.$vars['namespace'].'.resources').'config'.DS,
 ]);
 
-/**
+/*
  * Autoloading.
  */
 $loader = new ClassLoader();
@@ -89,19 +92,19 @@ foreach ($classes as $prefix => $path) {
 }
 $loader->register();
 
-/**
+/*
  * Register plugin public assets folder [dist directory].
  */
 container('asset.finder')->addPaths([
-    plugins_url('dist', __FILE__) => themosis_path('plugin.'.$vars['namespace']).'dist'
+    plugins_url('dist', __FILE__) => themosis_path('plugin.'.$vars['namespace']).'dist',
 ]);
 
-/**
+/*
  * Register plugin views.
  */
 container('view.finder')->addLocation(themosis_path('plugin.'.$vars['namespace'].'.resources').'views');
 
-/**
+/*
  * Service providers.
  */
 $providers = container('config')->get($vars['config'].'_providers');
@@ -110,6 +113,30 @@ foreach ($providers as $provider) {
     container()->register($provider);
 }
 
-/**
+/*
+ * Admin files autoloading.
+ * I18n.
+ */
+container('action')->add('plugins_loaded', function () use ($vars) {
+
+	/**
+	 * I18n
+	 * Todo: #2 - Replace constant below.
+	 */
+	load_plugin_textdomain(MY_PLUGIN_TD, false, trailingslashit(dirname(plugin_basename(__FILE__))).'languages');
+
+    /*
+     * Plugin admin files.
+     * Autoload files in alphabetical order.
+     */
+    $loader = container('loader')->add([
+        themosis_path('plugin.'.$vars['namespace'].'.admin'),
+    ]);
+
+    $loader->load();
+
+});
+
+/*
  * Add extra features below.
  */
